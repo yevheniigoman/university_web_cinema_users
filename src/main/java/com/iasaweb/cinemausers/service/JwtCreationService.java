2a0dbf0b742c525f05839b1501e87aa3034305ec
yevheniigoman <zhgoman@gmail.com>
2026-04-11 00:34:03 +0300

    added method for serializing user roles into list of strings in JwtService

--- src/main/java/com/iasaweb/cinemausers/service/JwtCreationService.java
+++ src/main/java/com/iasaweb/cinemausers/service/JwtCreationService.java
@@ -3,20 +3,24 @@ package com.iasaweb.cinemausers.service;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.List;
+import java.util.Collection;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.ZoneId;
+import java.util.stream.Collectors;
 
 @Service
-public class JwtService {
+public class JwtCreationService {
     private final SecretKey signingKey;
 
-    public JwtService(@Value("${secret}") String secret) {
+    public JwtCreationService(@Value("${secret}") String secret) {
         signingKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
     }
 
@@ -25,11 +29,18 @@ public class JwtService {
                 .atZone(ZoneId.systemDefault())
                 .plusHours(24);
 
+        List<String> authorities = serializeAuthorities(user.getAuthorities());
         return Jwts.builder()
                 .subject(user.getUsername())
-                .claim("roles", user.getAuthorities())
+                .claim("roles", authorities)
                 .expiration(Date.from(expirationTime.toInstant()))
                 .signWith(signingKey)
                 .compact();
+    }
+
+    private List<String> serializeAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        return authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
     }
 }
